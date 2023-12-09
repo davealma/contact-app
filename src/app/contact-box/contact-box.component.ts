f6015d3545ab3f0adaaf2fff2733bcdea4915c8a
David <davidalmanza88@gmail.com>
2023-12-09 00:46:08 -0600

    redirect to view contact

--- src/app/contact-box/contact-box.component.ts
+++ src/app/contact-box/contact-box.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from "@angular/core";
 import { Contact } from "../../models/models";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: "wt-contact-box",
     standalone: true,
+    imports: [RouterLink],
     templateUrl: './contact-box.component.html',
     styleUrl:'./contact-box.component.scss'
 })
