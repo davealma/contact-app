34212568a26e929dee8d30331e82d2b955b02a16
David <davidalmanza88@gmail.com>
2023-12-10 20:10:09 -0600

    Add image to list component

--- src/app/contact-box/contact-box.component.ts
+++ src/app/contact-box/contact-box.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { Contact } from "../../models/models";
 import { RouterLink } from "@angular/router";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
 
 @Component({
     selector: "wt-contact-box",
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, NgOptimizedImage, CommonModule],
     templateUrl: './contact-box.component.html',
     styleUrl:'./contact-box.component.scss'
 })
